ef1232d5568f326e3d2477add5e098127cabceee
Matan Kotler-Berkowitz <205matan@gmail.com>
2021-11-09 16:39:21 -0500

    Update website

---
+++ script.js
@@ -0,0 +1,62 @@
+particlesJS("header", {
+  canvas: {
+    h: "200px"
+  },
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000"
+      },
+      polygon: {
+        nb_sides: 5
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+    },
+    size: {
+      value: 3,
+      random: true,
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 0.5,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: false },
+      onclick: { enable: false },
+      resize: true
+    },
+  },
+  retina_detect: true
+});
